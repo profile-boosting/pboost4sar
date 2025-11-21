660bc63f3e5aaf0412f69a3e6bb13299e53f8148
zengc <zengc.xu@aliyun.com>
2025-11-21 13:19:28 +0800

    update `simu-data.R`

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,13 @@
+# pboostsam 0.2.1 (2025-11-21)
+
+* add `simu_sam_data_case()` in `simu-sam-data.R`.
+
+
+
 # pboostsam 0.2.0 (2025-11-21)
 
 * fix the generation of rook matrix in `simu-sam-data.R`.
+
 
 
 # pboostsam 0.1.0 (2025-11-20)
--- R/simu-sam-data.R
+++ R/simu-sam-data.R
@@ -1,6 +1,9 @@
+#' @name simu-data
 #' @title Simulate Data
 #' 
-#' @description Rook weight matrix.
+#' @description 
+#' - `simu_sam_data_rook`: Rook weight matrix.
+#' - `simu_sam_data_case`: Case weight matrix.
 #' 
 #' @param b0 True vector of \eqn{\beta_0}.
 #' * `c(1.5, 3.0, 2.0, rep(0.0, 3))`
@@ -9,7 +12,17 @@
 #' @param rho0 \eqn{\rho_0}.
 #' @param sig0 \eqn{\sigma_0} for \eqn{\varepsilon}.
 #' @param n Sample size.
+#' @param m In Case spatial weight matrix, \eqn{W = I_R \otimes B_m}.
+#' @param R In Case spatial weight matrix, \eqn{W = I_R \otimes B_m}.
 #' 
+#' @return `list(y, X, W0, s0)`.
+#' 
+NULL
+#> NULL
+
+
+#' @rdname simu-data
+#' @order 1
 #' @export
 simu_sam_data_rook <- function(b0, rho0, sig0, n) {
 
@@ -36,4 +49,24 @@ simu_sam_data_rook <- function(b0, rho0, sig0, n) {
     y <- solve(diag(n) - rho0*W0, as.matrix(X) %*% b0 + rnorm(n, sd=sig0))
 
     return( list(y=y, X=X, W0=W0, s0=names(X)[which(b0 != 0)]) )
+}
+
+
+
+#' @rdname simu-data
+#' @order 2
+#' @export 
+simu_sam_data_case <- function(b0, rho0, sig0, m, R=8) {
+
+    n <- m * R
+    p <- length(b0)
+    X <- data.frame( matrix(rnorm(n*p), n) %*% chol(0.7^abs(outer(1:p, 1:p, "-"))) )
+
+    W0 <- kronecker(diag(R), (matrix(1.0, m, m) - diag(m)) / (m - 1.0))
+    W0 <- W0 / rowSums(W0)
+
+    y <- solve(diag(n) - rho0*W0, as.matrix(X) %*% b0 + rnorm(n, sd=sig0))
+
+    return( list(y=y, X=X, W0=W0, s0=names(X)[which(b0 != 0)]) )
+
 }
